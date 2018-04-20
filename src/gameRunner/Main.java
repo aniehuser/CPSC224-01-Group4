4b29481b7faf54fe55dbd08f193abb388534f801
Anthony Niehuser <tniehuser@gmail.com>
2018-04-20 13:54:10 -0700

    added game logic example in main.java

--- src/gameRunner/Main.java
+++ src/gameRunner/Main.java
@@ -36,77 +36,88 @@ public class Main {
 	public static void main(String[] args) {
 		//Start Game
 		game.start();
-		
+	
 		//if problem starting game, ends program
 		if(!game.isValidInstance()){
 			System.out.println("Invalid Instance of Game object");
 			return;
 		}
+		
+		// intialize a player
 		Player p = new Player(game.getDieSides(),
 							   game.getDieNum(), 
 							   game.getRollsPerRound(), 
 							   1);
 		
-		p.rollInit();
-		System.out.println(p.getHand().toString());
-		if(!p.isRoundOver()){
-			p.rollOnce(genKeep());
+		// loop game until total rounds played == maxRounds (18). Do not increment round until
+		// every player has completed their round. I may edit the Game object to make these checks
+		// easier
+		while(game.isGameOver()){
+			
+			// start a round by calling p.rollInit()
+			p.rollInit();
+			
+			// get player's hand object by calling getHand()
 			System.out.println(p.getHand().toString());
-		}
-		if(!p.isRoundOver()){
-			p.rollOnce(genKeep());
-			System.out.println(p.getHand().toString());
-		}
-		// should not execute
-		if(!p.isRoundOver()){
-			p.rollOnce(genKeep());
-			System.out.println(p.getHand().toString());
-		}
-		
-		if(p.isRoundOver()){
-			System.out.println(p.toString());
-			System.out.println(p.getScorer().toString());
-
-			String keepScore = "3 of a Kind";
-			if(p.isScoreSet(keepScore))
-				p.setScore(keepScore);
-			System.out.println(p.toString());
-		}
-		
-		if(true) return;
-		boolean goodInput = false;
-		StringContainer inStr = new StringContainer();
-		Player p1;
-		
-		//Start Game
-		game.start();
-		
-		//if problem starting game, ends program
-		if(!game.isValidInstance()){
-			System.out.println("Invalid Instance of Game object");
-			return;
-		}
-		
-		// Update config file if specified by user
-		System.out.println(game.toString());
-		while(!goodInput){
-			goodInput = InputHandler.updateConfig(inStr);
-		}
-		if(inStr.getString().toLowerCase().equals("y")){
-			game.update();
-		}
-		
-		// Create Player
-		p1 = new Player(game.getDieSides(), game.getDieNum(), game.getRollsPerRound(), 1);
-		
-		// Game loop
-		while(game.getCurrentRound()<game.getMaxRounds()){
-			p1.doRound();
+			
+			//check if the round is over before rolling again.
+			// NOTE:: This check occurs automatically inside of rollOnce(). Method just
+			// returns without performing any functionality if constraints not met.
+			if(!p.isRoundOver()){
+				//After rollinit(), will use rollOnce() until the round is over. rollOnce()
+				// checks if the user keeps all cards and automatically sets the round to be 
+				// over. genKeep() generates a random boolean array which specifies which die to
+				// keep. True to keep, false to reroll. indices of input directly correspond to indice
+				// of die to keep
+				p.rollOnce(genKeep());
+				
+				System.out.println(p.getHand().toString());
+			}
+			
+			// perform another roll in the same fashion as before
+			if(!p.isRoundOver()){
+				p.rollOnce(genKeep());
+				System.out.println(p.getHand().toString());
+			}
+			
+			// this if statement should not execute due to the current state of the player object.
+			// I put it in to show that these statements are handled by internal logic of player object
+			if(!p.isRoundOver()){
+				p.rollOnce(genKeep());
+				System.out.println(p.getHand().toString());
+			}
+			
+			// if round is over, then do end of round calculations. In actual game, this should be implemented
+			// as a do while loop, until the user picks a score that they havent already chosen.
+			if(p.isRoundOver()){
+				// to print out players current score, call the player objects toString() method
+				System.out.println(p.toString());
+				
+				// you can get the possible scores by calling getScorer. You can output this data using
+				// the Score objects toString() method
+				System.out.println(p.getScorer().toString());
+	
+				// have player indicate which score they want to keep. the keys are strings as follows:
+				// {"1","2","3","4","5","6","7", "3 of a Kind", "4 of a Kind", "Full House", "The North",
+				// "The South", "Easteros", "The Dead", "The Crown", "The Others", "Dragons", "Yahtzee" }
+				String keepScore = "3 of a Kind";
+				
+				// first check to make sure the player hasnt already assigned the score to their scorecard.
+				// this step is important because due to our special rules, i will not be doing checks in
+				// setScore(key) to make sure that an already assigned score 
+				if(p.isScoreSet(keepScore)){
+					
+					// set the player's score by inputting the string key of what the user chose
+					p.setScore(keepScore);
+				}
+				
+				// output updated score again using toString.
+				System.out.println(p.toString());
+			}
+			
+			// after a round is over, increment the Game object's round to track game state. 
 			game.incrementRound();
 		}
-		
-		// print final score
-		System.out.println(p1.toString());
 		
 		// close globals
 		in.close();
