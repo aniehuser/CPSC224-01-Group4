ea7fa44498ce0b21f16cc9d0ee5b84f6dada0d50
Nicole Howard <nicolehoward@Nicoles-MacBook-Pro.local>
2018-04-30 22:03:13 -0700

    fixed instructions

--- src/gameUI/InstructionScreen.java
+++ src/gameUI/InstructionScreen.java
@@ -26,6 +26,21 @@ public class InstructionScreen {
      */
     public void start(final Stage primaryStage)  {
 
+        String dice = String.format("%-20s %s" , "Nights Watch Ranger" , "-- Jon Snow, 5% chance.\n" );
+        String dice1 = String.format("%-22s %s" , "Kingsguard Knight"  ,   "-- Jaime Lannister, 4% chance.\n");
+        String dice2 = String.format("%-24s %s" , "Faceless Man" , "-- Arya Stark, 3% chance.\n" );
+        String dice3 = String.format("%-26s %s" , "Undead" , "-- Undead Dragon, 2% chance.\n");
+        String dice4 = String.format("%-28s %s" , "Wildfire" , "-- Cersei, 1% chance.\n" );
+        String dice5 = String.format("%-24s %s" , "White Walker" , "-- The Night King, .75% chance.\n" );
+        String dice6 = String.format("%-26s %s" , "Drogon" , "-- Drogon, .5% chance.\n" );
+        System.out.println(dice);
+        System.out.println(dice1);
+        System.out.println(dice2);
+        System.out.println(dice3);
+        System.out.println(dice4);
+        System.out.println(dice5);
+        System.out.println(dice6);
+
         final String instructions = "How To Win:\n" + "Players pick a faction to " +
                 "belong to and fight the others by rolling the dice to become the ruling faction\n" +
                  "\n" +"Rules:\n" +
@@ -34,13 +49,7 @@ public class InstructionScreen {
                  "Each side of the dice has a representation of a warrior or \n" +
                 "character from Game of Thrones.  In each round, you may choose \nto reroll " +
                 "any number of your dice two times unless otherwise specified by a special rule.\n" + "\nThe Dice:\n" +
-                "Nights Watch Ranger -- Jon Snow         -- 5% chance \n" +
-                "Kingsguard Knight     -- Jaime Lannister -- 4% \n" +
-                "Faceless Man            -- Arya Stark       -- 3% \n" +
-                "Undead                -- Undead Dragon   -- 2% \n" +
-                "Wildfire            -- Cersei          -- 1%\n" +
-                "White Walker        -- The Night King  -- .75%\n" +
-                "Dragon              -- Drogon          -- .5%";
+                dice + dice1 +  dice2 + dice3 + dice4 + dice5 + dice6;
 
         final String scoring = "Scoring:\nEach turn you get pick one score to fill out on scorecard.\n" +
                 "\nUpper Scoring:\n" + "Each dice representation has a chance of getting a rare of that dice.\n" +
